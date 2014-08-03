37b7a09b492e69ddebaafca7c2f4170be4802e2b
Chase Wang <chase@ocwang.com>
2014-08-03 18:34:36 -0400

    Added pictures and smooth scroll to menu.

--- js/main.js
+++ js/main.js
@@ -37,6 +37,22 @@ $(document).ready(function(){
     }
 });
 
+// Smooth scrolling for anchor links on the page
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 // Resize Splash div to match window size
 function resizeSplash() {
   var h = $(window).height();
